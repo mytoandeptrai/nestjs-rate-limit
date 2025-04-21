0eb0bc0c2c1a780cddc70791a2ee6cd3246d8184
mytoandeptrai <67469007+mytoandeptrai@users.noreply.github.com>
2025-04-21 10:27:36 +0700

    feat: cors v2

--- src/main.ts
+++ src/main.ts
@@ -23,7 +23,6 @@ async function bootstrap() {
   app.enableCors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
   });
 
   await app.listen(process.env.PORT ?? 3000);
