9bebd7737a2697d9277077d4271f726bb0521cfe
mytoandeptrai <67469007+mytoandeptrai@users.noreply.github.com>
2025-04-21 10:27:23 +0700

    feat: cors

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,12 @@ async function bootstrap() {
     },
   });
 
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
